3d02d7356ce02af3c1f3e1d19819a29735a8bda6
Nirav.Patel <Nirav.Patel@BLR-6WW9L13-L>
2022-11-09 14:10:32 +0530

    Add project files.

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("CRUD Operations")]
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// This is to Fetch details
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        [HttpGet("Fetch User Details")]
+        public User FetchUserDetails(User employee)
+        {
+            employee.userCurrentProject = "";
+            return employee;
+        }
+
+        /// <summary>
+        /// This is to add details
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        [HttpPost("Add User Details")]
+        public User AddUserDetails(User employee)
+        {
+            employee.userName = "Nirav";
+            employee.userDoJ = "June 16rd, 2000";
+            employee.userId = 1405;
+            employee.userCurrentProject = "None";
+
+            return employee;
+        }
+
+        /// <summary>
+        /// This is to update details
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        [HttpPut("Update User Details")]
+        public User UpdateUserDetails(User employee)
+        {
+            employee.userName = "Nirav Patel";
+
+            return employee;
+        }
+
+        /// <summary>
+        /// This is to remove details
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        [HttpDelete("Remove User Details")]
+        public User RemoveEmployeeDetails(User employee)
+        {
+            employee.userName = "Nirav";
+            employee.userDoJ = "June 16rd, 2000";
+            employee.userId = 1405;
+
+            return employee;
+        }
+    }
+}
---
+++ WebApi/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi
+{
+    public class User
+    {
+        public string userName { get; set; }
+
+        public int userId { get; set; }
+
+        public string userDoJ { get; set; }
+
+        public string userCurrentProject { get; set; }
+    }
+}
